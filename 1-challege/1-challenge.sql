c808da7967ce377103287d41051e1031a5af0e5c
garu12 <garu12@hotmail.com>
2021-02-08 14:18:56 -0500

    1-challenge-Revisando-la-consulta-select

--- 1-challege/1-challenge.sql
+++
@@ -1,9 +0,0 @@
---Query a count of the number of cities in CITY having a Population larger than 100,000
---Input Format
-
---The CITY table is described as follows:
-
---En la carpeta 1-challenge esta la descripcion del problema
--- Solution
-
-SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
